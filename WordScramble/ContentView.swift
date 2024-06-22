92f5aa4bc350cb9fa6f98bb8962540ed1feec00d
estersr <estherramoos@gmail.com>
2024-06-22 14:09:44 -0300

    Animation, functions

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,18 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    func testStrings() {
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id:\.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
     }
     
-    var body: some View {
-        VStack {
-            
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //exit if the remaining string is empty
+        guard answer.count > 0 else {return}
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
         }
+        newWord = ""
     }
 }
 
